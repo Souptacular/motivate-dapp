ba267a9adfd650ce6f2bbd430182d5cd3e6c1351
tjade273 <tjade273@gmail.com>
2015-12-02 06:01:59 +0000

    Contract scaffolding

---
+++ contracts/motivate.sol
@@ -0,0 +1,15 @@
+contract Motivate {
+    address registry;
+    
+    function newGoal (bytes ipfs ) public {
+        
+    }
+    
+    function registerUser() public {
+        
+    }
+    
+    function 
+    
+    
+}
