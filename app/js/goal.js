46e60e37183803f2d46fc7a70f9a1c012fd0df3f
Richard <richardxlin@gmail.com>
2015-12-06 02:40:56 -0500

    feat(goalcreator): add front-end creation logic

--- app/js/goal.js
+++ app/js/goal.js
@@ -2,78 +2,114 @@
 
 angular.module('GoalCreator', ['ngRoute'])
 
-.controller('goalCtrl', function($scope){
+.controller('goalCtrl', function($scope, goalSubmissionService, goalTypeService){
   $scope.goal = {}; //Container for goal setup data
-  $scope.page = {}; //Containerfor page data 
+  $scope.goal.list = goalTypeService();
+  $scope.goal.data = {}; //Container for goal json data
+  $scope.goal.data = goalSubmissionService().get(); //Container for goal json data
 
-  //should be moved to a factory
-  $scope.page.goal = [
-   {
-    type: 'Writing',
-    tasks: [
-      {
+  $scope.goal.review = goalSubmissionService().get(); //Container for review page
 
-      }
-    ],
-    validations: [
-      {
+  $scope.goal.submitGoal = function(){
+    console.log('submitting goal: ', $scope.goal.data);
+  }
 
-      }
-    ] 
-   }
-  ];
+  $scope.updateDataFactory = function(stage){
+    goalSubmissionService().set(stage, $scope.goal.data);
+  }
 
 })
 
-.factory('goalService', function(){ 
+.factory('goalTypeService', function(){ 
 
-  //mock goal JSON - this will need to be stored and fetch
-  var goal = {
-    name: null,
-    description: null,
-    contact: null,
-    ethereumAddress: null,
-    goalTypes: [
-       {
-        type: 'Writing', //categorical type of goal
-        tasks: [ //list of preset tasks
-          {
-            type: 'Blogging'
-          }
-        ],
-        validations: [ //list of preset validations
-          {
-            type: 'word count',
-            startDate: null,
-            endDate: null,
-            count: null,
-            validatorUrl: 'validators/word-count.html'
-          }
-        ],
-        incentives: [ //list of incentives
-          {
-            type: 'standard-monetary',
-            deposit: null,
-            unit: null //unit of deprecation per failed task
-          }
-        ] 
-       }
+  var service = {
+    get: function() {
+      return this.data;
+    },
+    set: function(data){
+
+    },
+    data:[
+    {
+      type: 'Writing', //categorical type of goal
+      validations: [ //list of preset validations
+        {
+          type: 'word count',
+          startDate: null,
+          endDate: null,
+          count: null,
+          validatorUrl: 'validators/word-count.html'
+        }
+      ],
+      incentives: [ //list of incentives
+        {
+          type: 'ethereum',
+          deposit: null
+        }
+      ] 
+     }
     ]
   }
+
+  return function(data){
+
+    if(data){
+      service.set(data);
+    }
+
+    return service.get();
+  }
+
+})
+
+.factory('goalSubmissionService', function(){ 
+
+  var service = {
+    get: function() {
+      return this.data;
+    },
+    set: function(stage, data){
+      if (stage === 'start') {
+        this.data.name = data.name;
+        this.data.description = data.description;
+        this.data.contact = data.contact;
+        this.data.ethereumAddress = data.ethereumAddress;
+      }
+
+      if (stage === 'setup') {
+        this.data.type = data.type;
+        this.data.incentive = data.incentive;
+        this.data.validation = data.validation;
+      }
+
+    },
+    data: {
+      name: null,
+      description: null,
+      contact: null,
+      ethereumAddress: null,
+      type: null,
+      incentive: null,
+      validation: null
+    }
+  }
+
   return function(){
-    return goal;
+    return service;
   }
 })
+
+
 
 .config(function($routeProvider) {
 
     $routeProvider
-      .when('/setup', {
-        templateUrl: '/app/partials/goal-setup.html',
+      .when('/start', {
+        templateUrl: '/app/partials/goal-start.html',
         controller: 'goalCtrl'
       })
-      .when('/accountability', {
-        templateUrl: '/app/partials/goal-accountability.html',
+      .when('/setup', {
+        templateUrl: '/app/partials/goal-setup.html',
         controller: 'goalCtrl'
       })
       .when('/review', {
@@ -81,7 +117,7 @@ angular.module('GoalCreator', ['ngRoute'])
         controller: 'goalCtrl'
       })
       .otherwise({
-        redirectTo: '/setup'
+        redirectTo: '/start'
       });
 
   })
