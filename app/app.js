4052841e7e4ff41d98df19b2eabc061db68fbd32
Richard <richardxlin@gmail.com>
2015-12-05 21:25:55 -0500

    feat(goal-creator): implement goal data struct - need more work

--- app/app.js
+++ app/app.js
@@ -4,6 +4,65 @@ angular.module('GoalCreator', ['ngRoute'])
 
 .controller('goalCtrl', function($scope){
 	$scope.testMsg = "Goal Creator";
+  $scope.goal = {}; //Container for goal setup data
+  $scope.page = {}; //Containerfor page data 
+
+  //should be moved to a factory
+  $scope.page.goal = [
+   {
+    type: 'Writing',
+    tasks: [
+      {
+
+      }
+    ],
+    validations: [
+      {
+
+      }
+    ] 
+   }
+  ];
+
+})
+
+.factory('goalService', function(){ 
+
+  //mock goal JSON - this will need to be stored and fetch
+  var goal = {
+    name: null,
+    description: null,
+    contact: null,
+    ethereumAddress: null,
+    goalTypes: [
+       {
+        type: 'Writing', //categorical type of goal
+        tasks: [ //list of preset tasks
+          {
+            type: 'Blogging'
+          }
+        ],
+        validations: [ //list of preset validations
+          {
+            type: 'word count',
+            startDate: null,
+            endDate: null,
+            count: null,
+          }
+        ],
+        incentives: [ //list of incentives
+          {
+            type: 'standard-monetary',
+            deposit: null,
+            unit: null //unit of deprecation per failed task
+          }
+        ] 
+       }
+    ]
+  }
+  return function(){
+    return goal;
+  }
 })
 
 .config(function($routeProvider) {
@@ -25,7 +84,7 @@ angular.module('GoalCreator', ['ngRoute'])
         redirectTo: '/setup'
       });
 
-  });
+  })
 
 ;
 
